0ae9ff9690d6eac5f6184cb07ddfa4690ea98183
lachlanc61 <lachlanc.612@gmail.com>
2023-09-06 22:47:24 +1000

    built state objects in logic.py, added tests

---
+++ backend/rimsboard/logic.py
@@ -0,0 +1,148 @@
+import copy
+
+class IState():
+    """
+    collection of valid states to hand back
+    """
+    def __init__(self):
+        #standard--------
+        self.off = 'off'
+        self.incomplete = 'incomplete'
+        self.waiting = 'waiting'
+        self.waiting_external = 'waiting_external'
+        self.ready = 'ready'
+        self.extended = 'extended'
+        self.disabled = 'disabled'
+        #error------------
+        self.warn = 'warn'
+        self.fail = 'fail'
+        self.na = 'na'
+
+ISTATE = IState()
+
+
+
+class IndicatorState():
+    """
+    class to hold state values, and lists of state values
+    """      
+    def __init__(self, key: str, state: str, label:str =None):
+        self.key = key
+        self.state = state
+
+        if label == None:
+            self.label = key        
+        else:
+            self.label = label
+    def to_dict(self):
+        """
+        return as nested dicts
+        """
+
+        return { 'key': self.key, 'label': self.label, 'state': self.state }
+
+
+class IndicatorStateGroup():
+    """
+    class to hold state values, and lists of state values
+    """      
+    def __init__(self, key: str, indicators: list, label:str =None):
+        self.key = key
+        self.indicators = indicators
+
+        if label == None:
+            self.label = key        
+        else:
+            self.label = label
+
+   
+    def group_check(self):
+        """
+        check if current instance is a state_group
+        REMOVE
+        """        
+        if not ( type(self.indicators) is list):
+            raise TypeError(f"State {self.key} is not group of states")
+
+    def add(self, indicator):   
+        """
+        add an instance of state if current instance is a state_group
+        """   
+
+        #recursive, takes instance of State(), not sure how to type hint
+        #   check by attributes
+        if ( hasattr(indicator, 'key') and hasattr(indicator, 'label') and hasattr(indicator, 'state') ):
+            for i in self.indicators:
+                if i.key == indicator.key:
+                    raise ValueError(f"State {indicator.key} already in group {self.indicators}")
+
+            self.indicators.append(indicator)     
+        else:
+            raise ValueError(f"{indicator.key} is not valid state to append")
+   
+
+    def assign(self, key: str, state):
+        """
+        assign value to state from key
+        NB: does not check state to be assigned
+        """        
+        for i in self.indicators:
+            if i.key == key:
+                i.state = state
+
+    def get_state(self, key):
+        """
+        return state from key
+        """
+
+        for i in self.indicators:
+            if i.key == key:
+                return(i.state)
+
+    def to_dict(self):
+        """
+        return as nested dicts
+        """
+
+        sub_result = [] 
+        for i in self.indicators:
+            sub_result.append(i.to_dict())
+
+        return { 'key': self.key, 'label': self.label, 'indicators': sub_result }
+
+    def flat(self):
+        """
+        flatten nested subgroups into parent
+        """        
+
+        _target = copy.deepcopy(self)
+
+        remove_list = []
+
+        for subgroup in _target.indicators:
+
+            #check if subcomponent is group
+            if ( hasattr(subgroup, 'indicators')):
+                try:
+                    #check for further nesting and flatten recursively if needed
+                    for substate in subgroup.indicators:
+                        if ( hasattr(substate, 'indicators')):
+                            subgroup = subgroup.flat()
+                            break   #only need to run flat() once, will flatten out all sub-subgroups
+
+                    #add sub-state to parent
+                    for substate in subgroup.indicators:        
+                        _target.add(substate)
+                    
+                    #flag subgroup for removal
+                    remove_list.append(subgroup)
+
+                except:
+                    print(f"WARNING: flattening state instance {_target}, {_target.key} failed at {subgroup.key}, {substate.key}")
+                    #remove any added js if there is a failure
+        
+        for subgroup in remove_list:
+            _target.indicators.remove(subgroup)
+
+        return _target
+
---
+++ backend/tests/test_indicator_states.py
@@ -0,0 +1,118 @@
+import sys, os
+
+#PATHS
+TEST_DIR=os.path.realpath(os.path.dirname(__file__))
+BASE_DIR=os.path.dirname(TEST_DIR)
+sys.path.append(BASE_DIR)
+
+from rimsboard.logic import IndicatorState, IndicatorStateGroup, ISTATE
+
+
+def utils_intialise_access():
+    lab1 = IndicatorState('analytical_chem', ISTATE.off )
+    lab2 = IndicatorState('radiography', ISTATE.fail, label="RAD")
+    lab3 = IndicatorState('materials_science', ISTATE.ready)
+
+    access = IndicatorStateGroup('Access', [lab1, lab2, lab3])
+    
+    return access
+
+def test_create_state():
+    lab1 = IndicatorState('analytical_chem', ISTATE.off )
+    lab2 = IndicatorState('radiography', ISTATE.fail, label="RAD")
+
+    expected1 = {'key': 'analytical_chem', 'label': 'analytical_chem', 'state': 'off'}
+    expected2 = {'key': 'radiography', 'label': 'RAD', 'state': 'fail'}
+
+    assert lab1.to_dict() == expected1
+    assert lab2.to_dict() == expected2
+
+def test_create_stategroup():
+    access = utils_intialise_access()
+
+    expected = {'key': 'Access',
+                'label': 'Access',
+                'indicators': [
+                    {'key': 'analytical_chem', 'label': 'analytical_chem', 'state': 'off'},
+                    {'key': 'radiography', 'label': 'RAD', 'state': 'fail'},
+                    {'key': 'materials_science', 'label': 'materials_science', 'state': 'ready'}
+                ]}    
+
+    result = access.to_dict()
+
+    assert result == expected
+
+def test_add_state():
+    lab4 = IndicatorState('softmatter', ISTATE.ready, label="soft")
+    access = utils_intialise_access()
+    
+    expected = {'key': 'Access',
+                'label': 'Access',
+                'indicators': [
+                    {'key': 'analytical_chem', 'label': 'analytical_chem', 'state': 'off'},
+                    {'key': 'radiography', 'label': 'RAD', 'state': 'fail'},
+                    {'key': 'materials_science', 'label': 'materials_science', 'state': 'ready'},
+                    {'key': 'softmatter', 'label': 'soft', 'state': 'ready'}
+                ]}  
+    
+    access.add(lab4)
+
+    result = access.to_dict()
+
+    assert result == expected    
+
+
+def test_add_subgroup():
+    uprop1=IndicatorState('user_ready', ISTATE.off)
+    uprop2=IndicatorState('has_account', ISTATE.ready)
+
+    access = utils_intialise_access()
+
+    user_properties=IndicatorStateGroup('Access', [uprop1, uprop2, access])
+
+    expected = {'key': 'Access',
+                'label': 'Access',
+                'indicators': [
+                    {'key': 'user_ready', 'label': 'user_ready', 'state': 'off'},
+                    {'key': 'has_account', 'label': 'has_account', 'state': 'ready'},
+                    {'key': 'Access',
+                    'label': 'Access',
+                    'indicators': [
+                        {'key': 'analytical_chem','label': 'analytical_chem','state': 'off'},
+                        {'key': 'radiography', 'label': 'RAD', 'state': 'fail'},
+                        {'key': 'materials_science','label': 'materials_science','state': 'ready'}
+                    ]}
+                ]}
+
+    result = user_properties.to_dict()
+
+    assert result == expected   
+
+def test_flatten_subgroup():
+    uprop1=IndicatorState('user_ready', ISTATE.off)
+    uprop2=IndicatorState('has_account', ISTATE.ready)
+
+    access = utils_intialise_access()
+
+    user_properties=IndicatorStateGroup('Access', [uprop1, uprop2, access])
+
+    expected = {'key': 'Access',
+                'label': 'Access',
+                'indicators': [
+                    {'key': 'user_ready', 'label': 'user_ready', 'state': 'off'},
+                    {'key': 'has_account', 'label': 'has_account', 'state': 'ready'},
+                    {'key': 'analytical_chem','label': 'analytical_chem','state': 'off'},
+                    {'key': 'radiography', 'label': 'RAD', 'state': 'fail'},
+                    {'key': 'materials_science','label': 'materials_science','state': 'ready'}
+                    ]
+                }   
+    
+    result = user_properties.flat()
+
+    result = result.to_dict()
+
+    assert result == expected    
+
+def test_flatten_recursive():
+    #to-do: check for double-stacked subgroups handled by recursive flattening in stategroup.flat()
+    pass
