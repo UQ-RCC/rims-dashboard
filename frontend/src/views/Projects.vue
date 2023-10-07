9d0ca92ededfae114386d0c8246fde58ea79bc4d
lachlanc61 <lachlanc.612@gmail.com>
2023-10-07 23:17:51 +1000

    projects page searchable by id, title, group

--- frontend/src/views/Projects.vue
+++ frontend/src/views/Projects.vue
@@ -9,16 +9,6 @@
         ></v-progress-linear>
         <div>
             <v-row>
-                <v-col cols="20" sm="8" md="5">
-                    <v-text-field
-                        v-model="filteredTitle"
-                        append-icon="mdi-magnify"
-                        label="Search By Title"
-                        single-line
-                        hide-details
-                        @keydown.enter.prevent="filterByTitle"
-                    ></v-text-field>
-                </v-col>                       
                 <v-col cols="12" sm="6" md="3">
                     <v-text-field
                         v-model="filteredId"
@@ -29,7 +19,27 @@
                         @keydown.enter.prevent="filterById"
                         :rules="numberRules"
                     ></v-text-field>
-                </v-col>            
+                </v-col> 
+                <v-col cols="20" sm="8" md="5">
+                    <v-text-field
+                        v-model="filteredTitle"
+                        append-icon="mdi-magnify"
+                        label="Search By Title"
+                        single-line
+                        hide-details
+                        @keydown.enter.prevent="filterByTitle"
+                    ></v-text-field>
+                </v-col>              
+                <v-col cols="12" sm="6" md="3">
+                    <v-text-field
+                        v-model="filteredGroup"
+                        append-icon="mdi-magnify"
+                        label="Search By Group"
+                        single-line
+                        hide-details
+                        @keydown.enter.prevent="filterByGroup"
+                    ></v-text-field>
+                </v-col>                        
             </v-row>                    
         </div>
         <v-data-table
@@ -73,7 +83,10 @@
 </template>
 
 <!--
-                        <td><v-icon large @click="deleteFob(item)"> mdi-access-point-remove </v-icon>
+    search field clear button
+    https://stackoverflow.com/questions/2803532/how-do-i-put-a-clear-button-inside-my-html-text-input-box-like-the-iphone-does
+    maybe just use v-autocomplete or v-search?
+    eg. https://codepen.io/AndrewThian/pen/QdeOVa
 -->
 
 <script>
@@ -95,9 +108,10 @@
                 loading: false,
 
                 projects: [],
-
+                projectsFull: [],
                 filteredId: null,
                 filteredTitle: null,
+                filteredGroup: null,
 
                 projectsTableHeaders: [
                     { text: 'Id', value: 'metadata.ProjectRef', width: '7%' },
@@ -115,13 +129,58 @@
             async refresh(){
                 Vue.$log.info("P refreshing ...")
                 this.loading = true
-                this.projects = await this.retrieveAllProjectStates()
+                this.projectsFull = await this.retrieveAllProjectStates()
                 //this.projects = await ProjectAPI.getProjects()
                 this.loading = false       
+                this.projects = this.projectsFull
                 Vue.$log.info("P refresh complete...")                                         
                 Vue.$log.info(this.projects)  
                 Vue.$log.info(this.projects[0].metadata.ProjectName)                 
             },
+
+            async filterById(){
+                this.projects = []
+                if (this.filteredId){
+                    this.filteredTitle = null
+                    this.filteredGroup = null                    
+                    this.projectsFull.forEach(aproj => {
+                    if(aproj.metadata.ProjectRef === parseInt(this.filteredId))
+                        this.projects = [aproj]
+                    })
+                } else {
+                    this.projects = this.projectsFull
+                }
+            },
+
+            async filterByTitle(){
+                this.projects = []
+                if (this.filteredTitle){
+                    this.filteredId = null
+                    this.filteredGroup = null
+                    this.projectsFull.forEach(aproj => {
+                    if(aproj.metadata.ProjectName.toLowerCase().includes(this.filteredTitle.toLowerCase()) )
+                        this.projects.push(aproj)
+                    })
+                } else {
+                    this.projects = this.projectsFull
+                }
+            },
+
+            async filterByGroup(){
+                this.projects = []
+                if (this.filteredGroup){
+                    this.filteredId = null
+                    this.filteredTitle = null
+                    this.projectsFull.forEach(aproj => {
+                        if(aproj.metadata.ProjectGroup.toLowerCase().includes(this.filteredGroup.toLowerCase()) )
+                            this.projects.push(aproj)
+                    })
+                } else {
+                    this.projects = this.projectsFull
+                }
+            },
+
+
 
             async retrieveDefaultUserState() {
                 let user_state = { };
@@ -167,7 +226,7 @@
                 return project_states
             },            
             
-            fill_default_headers() {
+            fillDefaultHeaders() {
                 //iterate through indicator labels in default state
                 //assign headers based on this
                 for (const item of this.default_project_state.indicator_labels) {
@@ -175,6 +234,12 @@
                 }
                 Vue.$log.info(this.projectsTableHeaders) 
             },
+
+            caseCompare(a, b) {
+                return typeof a === 'string' && typeof b === 'string'
+                    ? a.localeCompare(b, undefined, { sensitivity: 'accent' }) === 0
+                    : a === b;
+           },
         },
         mounted: async function() {
             Vue.$log.info("waiting")
@@ -182,7 +247,7 @@
             //sleep 5 sec
             await new Promise(r => setTimeout(r, 2000));
 
-            this.fill_default_headers()
+            this.fillDefaultHeaders()
 
             this.refresh()
             Vue.$log.info("recieved")            
