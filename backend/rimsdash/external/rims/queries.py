20d8607c7eaf6494f66c65ce53a6fb446887ad9e
lachlanc61 <lachlanc.612@gmail.com>
2024-01-12 14:59:17 +1000

    added training request fetcher

--- backend/rimsdash/external/rims/queries.py
+++ backend/rimsdash/external/rims/queries.py
@@ -412,147 +412,13 @@ def get_project_users(projectid: int):
     return []
 
 
-
-
-#------------------------------------------
-#pitschi originals
-#------------------------------------------
-
-def pts_get_ppms_user(login):
-    url = f"{config.get('ppms', 'ppms_url')}pumapi/"
-    key=f"{config.get('ppms', 'api2_key')}"
-    payload=f"apikey={key}&action=getuser&login={login}&format=json"
-    
-    
-    headers = {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    response = requests.request("POST", url, headers=headers, data=payload)
-    if response.ok:
-        if response.status_code == 204:
-            raise Exception('Not found')
-        else:
-            # logger.debug(f"Response: {response}")
-            return response.json(strict=False)
-    else:
-        raise Exception('Not found')
-
-
-def pts_get_ppms_user_by_id(uid:int, coreid:int):
-    logger.debug("@get_ppms_user_by_id: Querying user by id")
-    url = f"{config.get('ppms', 'ppms_url')}API2/"
-    key=f"{config.get('ppms', 'api2_key')}"    
-    payload=f"outformat=json&apikey={key}&action=GetUserDetailsById&checkUserId={uid}&coreid={coreid}"
-    
-    headers = {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    response = requests.request("POST", url, headers=headers, data=payload)
-    if response.ok:
-        if response.status_code == 204:
-            return []
-        else:
-            return response.json(strict=False)
-    return []
-
-
-def pts_get_system_rights(systemid: int):
-
-    url = f"{config.get('ppms', 'ppms_url')}pumapi/"
-    key=f"{config.get('ppms', 'api2_key')}"
-
-    payload=f"apikey={key}&action=getsysrights&id={systemid}"
-    print(payload)
-    headers = {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    response = requests.request("POST", url, headers=headers, data=payload)
-    if response.ok:
-        if response.status_code == 204:
-            return {}
-        else:
-            # format is in mode:name\n
-            _system_rights_text = response.text.strip()
-            _lines = _system_rights_text.split('\n')
-            _permissions = { _line.split(":")[1]:_line.split(":")[0] for _line in _lines }
-            return _permissions
-    return {}
-
-
-def pts_get_project_users(projectid: int):
-    logger.debug("Querying project user")
-    url = f"{config.get('ppms', 'ppms_url')}pumapi/"
-    payload=f"apikey={config.get('ppms', 'api2_key')}&action=getprojectusers&withdeactivated=false&projectid={projectid}"
-    headers = {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    response = requests.request("POST", url, headers=headers, data=payload)
-    if response.ok:
-        if response.status_code == 204:
-            return []
-        else:
-            response_txt = response.text
-            return response_txt.strip().split("\r\n")
-    else:
-        return []
-
-def pts_get_project_members(projectid: int):
+def get_training_requests() -> list[dict]:
     """
-    Similar to project_user but with user id as well
-    """
-    logger.debug("Querying project user")
-    url = f"{config.get('ppms', 'ppms_url')}pumapi/"
-    payload=f"apikey={config.get('ppms', 'api2_key')}&action=getprojectmember&projectid={projectid}"
-    headers = {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    response = requests.request("POST", url, headers=headers, data=payload)
-    if response.ok:
-        if response.status_code == 204:
-            return []
-        else:
-            response_txt = response.text
-            _csv_reader = csv.reader(response_txt.split('\n'), delimiter=',')
-            _csv_reader.__next__()
-            members = []
-            for row in _csv_reader:
-                if(len(row) > 8):
-                    _userid = int(row[1])
-                    _userlogin = row[8]
-                    members.append({'id': _userid, 'login': _userlogin})
-            return members
-    else:
-        return []
-    
-def pts_get_rdm_collection(coreid: int, projectid: int):
-    url = f"{config.get('ppms', 'ppms_url')}API2/"
-    payload=f"apikey={config.get('ppms', 'api2_key')}&action={config.get('ppms', 'qcollection_action')}&projectId={projectid}&coreid={coreid}&outformat=json"
-    headers = {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    response = requests.request("POST", url, headers=headers, data=payload)
-    if response.ok:
-        if response.status_code == 204:
-            return ""
-        qcollection = ""
-        if len(response.json()) > 0:
-            qcollection = response.json(strict=False)[0].get(config.get('ppms', 'q_collection_field'))
-        return qcollection
-    return ""
-
-"""
-ALTERNATIVES
-"""
-def get_userlist_api2():
-    """
-    requests user report from RIMS API
-    returns json
-    returns login, names, coreid, id
+    fetches list of training requests from RIMS
     """    
-    REPORT_NO=1335  #user list
     url=f"{BASE_URL}API2/"
     return_format=f"json"
-    payload=f"apikey={KEY}&action=GetUsersListJsonDB&dateformat=print&outformat={return_format}&coreid={CORE_ID}"
+    payload=f"apikey={KEY}&action=GetTrainingRequestsList&dateformat=print&outformat={return_format}&coreid={CORE_ID}"
     headers = {
       'Content-Type': 'application/x-www-form-urlencoded'
     }
@@ -564,35 +430,5 @@ def get_userlist_api2():
             raise Exception('Not found')
         else:
             return response.json(strict=False)
-    else:
-        raise Exception('Not found')
-
-def get_userlist_pumapi(active_only=False):
-    """
-    requests user report from RIMS API
-    returns json
-    returns only user ids
-    """    
-    url = f"{BASE_URL}pumapi/"
-    return_format=f"json"
-
-    if active_only:
-        payload=f"apikey={KEY}&action=getusers&active=True&format={return_format}"
-    else:
-        payload=f"apikey={KEY}&action=getusers&format={return_format}"
-
-    headers = {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-    if response.ok:
-        if response.status_code == 204:
-            raise Exception('Not found')
-        else:
-            logger.debug(f"Response: {response}")
-            response_txt = response.text
-            return response_txt.strip().split("\r\n")            
-            #return response.json(strict=False)
     else:
         raise Exception('Not found')
