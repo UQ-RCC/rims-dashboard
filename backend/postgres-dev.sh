853ec9a1f622fb04c30b8917965897a0f661104f
Lachlan Casey <lachlanc.612@gmail.com>
2024-03-08 13:23:56 +1000

    [BUGFIX] corrected unassigned db port, improved readability of dockerfile copy

--- backend/postgres-dev.sh
+++ backend/postgres-dev.sh
@@ -7,4 +7,4 @@ db_name=$(awk -F "=" '/db_name/ {print $2}' $CONF_FILE | tr -d ' ' )
 db_username=$(awk -F "=" '/db_username/ {print $2}' $CONF_FILE | tr -d ' ' )
 db_pw=$(awk -F "=" '/db_password/ {print $2}' $CONF_FILE | tr -d ' ' )
 
-docker run --rm -e POSTGRES_DB=$db_name -e POSTGRES_USER=$db_username -e POSTGRES_PASSWORD=$db_pw -v $DB_VOLUME:/var/lib/postgresql/data/ -p mydbport:mydbport postgres:12
+docker run --rm -e POSTGRES_DB=$db_name -e POSTGRES_USER=$db_username -e POSTGRES_PASSWORD=$db_pw -v $DB_VOLUME:/var/lib/postgresql/data/ -p 5432:5432 postgres:12
